5b146eedf73753ee748413b368ba3c16d6160423
nolte <nolte07@googlemail.com>
2020-08-07 19:13:22 +0200

    add helm test files

---
+++ magefile.go
@@ -0,0 +1,14 @@
+//+build mage
+
+package main
+
+import (
+
+
+	// mage:import
+	_"github.com/nolte/plumbing/cmd/kind"
+)
+
+//var Deployment mageUtils.HelmDeployment
+
+//var Deployment mageUtils.HelmDeployment
